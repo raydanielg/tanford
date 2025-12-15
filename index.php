71929e449b1a03bb529df92cd74a961ea0f7e015
raydanielg <airezra2+1@gmail.com>
2025-12-15 19:57:29 +0300

    commit

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 // Simple front controller to redirect all requests to Laravel's public/index.php
 // Useful when your hosting document root points to the project root instead of /public.
-
+// yaani imenites - this file acts as a bridge to the Laravel public directory
 // If you want to hard-code a subdirectory, adjust this path accordingly.
 require __DIR__ . '/public/index.php';
