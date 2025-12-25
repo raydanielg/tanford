1b6db49e62950fc39aef676589903e8a3adb0364
raydanielg <airezra2+1@gmail.com>
2025-12-25 22:03:42 +0300

    commit

--- public/build/assets/ApplicationLogo-DsIGdvdD.js
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:public/build/assets/ApplicationLogo-BgvaR_Dr.js
-import{_ as o}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{c,o as t}from"./app-lIrt8iGD.js";const n={},r={src:"/tanford.logo.png",alt:"Tanford logo",class:"h-8 w-8 object-contain"};function e(s,a){return t(),c("img",r)}const f=o(n,[["render",e]]);export{f as A};
-========
-import{_ as o}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{c,o as t}from"./app-oJqD9MA5.js";const n={},r={src:"/tanford.logo.png",alt:"Tanford logo",class:"h-8 w-8 object-contain"};function e(s,a){return t(),c("img",r)}const f=o(n,[["render",e]]);export{f as A};
->>>>>>>> cc366a5f08583de70e2dcb227634d293ab7574de:public/build/assets/ApplicationLogo-DsIGdvdD.js
